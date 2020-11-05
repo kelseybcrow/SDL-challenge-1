9b80918f6ac2e516e931e46007b4df9ae5eb2258
Kelsey Crow <kelseybcrow@gmail.com>
2020-11-05 12:06:50 -0700

    progress

---
+++ challenge1.js
@@ -0,0 +1,13 @@
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var assert = require('chai').assert;
+
+describe("challenge1 suite", function() {
+  before(function () {
+
+  });
+
+  after(function () {
+
+  });
+});
